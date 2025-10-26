d5f13f218f4c586c9b2ee20672c247f8e6c2c998
Camila Arenas <mcarenasdh@gmail.com>
2025-10-26 12:52:40 +0100

    chore: stop tracking ignored files

--- prisma/migrations/20251017170518_add_drawing_comments/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `Comment` ADD COLUMN `drawingId` VARCHAR(191) NULL,
-    MODIFY `logEntryId` VARCHAR(191) NULL;
-
--- AddForeignKey
-ALTER TABLE `Comment` ADD CONSTRAINT `Comment_drawingId_fkey` FOREIGN KEY (`drawingId`) REFERENCES `Drawing`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
